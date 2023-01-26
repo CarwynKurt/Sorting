486706cbc5beb7ec002788401ac45bed473fc1b3
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-27 00:50:12 +0800

    Merge

--- Merge Sort.py
+++ Merge Sort.py
@@ -9,5 +9,29 @@ def merge_sort(nums):
         merge_sort(left_array)
         merge_sort(right_array)
 
-# Merge
+        # Merge
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                nums[k] = left_array[i]
+                i += 1
+            else:
+                nums[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i < len(left_array):
+            nums[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            nums[k] = right_array[j]
+            j += 1
+            k += 1
+
+        print(nums)
+
 # Present Array Values
