04466cc541a5bd9892f94c54ada53a5609c92052
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-27 01:51:47 +0800

    While Loop

--- Quick Sort.py
+++ Quick Sort.py
@@ -11,5 +11,20 @@ def partition(nums, left, right):
     j = right - 1
     pivot = nums[right]
 
-# While Loop
+    # While Loop
+    while i < j:
+        while i < right and nums[i] < pivot:
+            i += 1
+        while j > left and nums[j] >= pivot:
+            j -= 1
+
+        if i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+
+    if nums[i] > pivot:
+        nums[i], nums[right] = nums[right], nums[i]
+
+    print(nums)
+    return i
+
 # Present Array Values
