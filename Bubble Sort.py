91c74b38cfd1ecf5dc7dfdc5546f92294f8122da
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-26 22:59:30 +0800

    Create Nested Loop

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,4 +1,14 @@
 # Define Sort
 def sort(nums):
-# Create Nested Loop
+
+    # Create Nested Loop
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+                print(nums)
+
 # Present Array Values
