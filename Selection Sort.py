f3e87f11cef5929ad7799b83c603b5cf07c53102
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-26 18:39:46 +0800

    Create Nested Loop

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,5 +1,17 @@
 # Define Sort
 def sort(nums):
 
-# Create loop
+    # Create Nested Loop
+    for i in range(9):
+        minpos = i
+        for j in range(i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
+
 # Present Array Values
