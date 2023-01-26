c0c7d0a65b063ad8f88d8b2b9b570a33259b7b17
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-27 01:41:36 +0800

    Define Partition

--- Quick Sort.py
+++ Quick Sort.py
@@ -6,5 +6,10 @@ def quick_sort(nums, left, right):
         quick_sort(nums, partition_pos + 1, right)
 
 # Define Partition
+def partition(nums, left, right):
+    i = left
+    j = right - 1
+    pivot = nums[right]
+
 # While Loop
 # Present Array Values
