cd02a139f55fb2fadfbf56217ce60fd33e7eef35
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-27 00:42:15 +0800

    Divide Array

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,6 +1,10 @@
 # Defines Merge Sort
 def merge_sort(nums):
-# Divide Array
+    # Divide Array
+    if len(nums) > 1:
+        left_array = nums[:len(nums) // 2]
+        right_array = nums[len(nums) // 2:]
+
 # Recursion
 # Merge
 # Present Array Values
